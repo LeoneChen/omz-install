fe83d444791526efa9abdeaf3616ace7e7cb4c72
小气球归来 <791960492@qq.com>
2024-06-26 21:35:21 +0800

    Update omz-install.sh

--- omz-install.sh
+++ omz-install.sh
@@ -2,13 +2,16 @@
 
 set -e
 
-export http_proxy="http://127.0.0.1:20171" && export https_proxy="http://127.0.0.1:20171"
-
 sed -i "s/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list
 sed -i "s/security.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list
 apt update
-apt install build-essential git zsh wget -y
+apt install build-essential git zsh wget sudo curl vim man-db -y
+unminimize
 
+useradd -m leone
+usermod -aG sudo leone
+
+export http_proxy="http://127.0.0.1:20171" && export https_proxy="http://127.0.0.1:20171"
 rm -rf ~/.oh-my-zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
